eae0aaf524ee9f87f09380cf569da0ccd26f9ad4
Bounphithak <bounphithakphithak@yahoo.com>
2025-06-01 18:35:05 +0200

    added snackBar to the registration/login

--- src/pages/registerLogin/registerLogin.page.ts
+++ src/pages/registerLogin/registerLogin.page.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
     selector: 'registerLogin-page',
     templateUrl: './registerLogin.page.html',
-    imports: [FormsModule]
+    imports: [FormsModule, MatSnackBarModule]
 })
 export class RegisterLoginPage {
     name = '';
@@ -17,7 +18,7 @@ export class RegisterLoginPage {
     loginName = '';
     loginPass = '';
 
-    constructor(private http: HttpClient, private router: Router, private authService: AuthService) {}
+    constructor(private http: HttpClient, private router: Router, private authService: AuthService, private snackBar : MatSnackBar) {}
 
     register() {
         const userData = {
@@ -30,11 +31,19 @@ export class RegisterLoginPage {
         this.http.post('http://localhost:3000/users', userData).subscribe({
             next: (res) => {
                 console.log('User registered:', res);
+                this.snackBar.open('User registered successfully! Please login!', 'Close', {
+                    duration: 3000,
+                    panelClass: ['bg-red-600', 'text-white'],
+                });
                 this.router.navigate(['/']);
             },
             
             error: (err) => {
                 console.error('Registration failed:', err.error);
+                this.snackBar.open('Registration failed! Try again.', 'Close', {
+                    duration: 3000,
+                    panelClass: ['bg-red-600', 'text-white'],
+                });
             }
         });
     }
@@ -50,10 +59,18 @@ export class RegisterLoginPage {
                 console.log('Login Successful:', res);
                 this.authService.setRole(res.role);
                 this.authService.setName(res.name);
+                this.snackBar.open('Login successful!', 'Close', {
+                    duration: 3000,
+                    panelClass: ['bg-red-600', 'text-white'],
+                });
                 this.router.navigate(['/']);
             },
             error: (err) => {
             console.error('Login failed:', err.error);
+            this.snackBar.open('Login failed! Try again.', 'Close', {
+                duration: 3000,
+                panelClass: ['bg-red-600', 'text-white'],
+            });
             }
         });
     }
